726f283c45f1b8dd0a540c5bc87edd397953ccb6
Divyanshu Agrawal <divyanshu123@hotmail.com>
2019-07-03 18:15:49 +0530

    Add login processing

--- src/App.js
+++ src/App.js
@@ -5,27 +5,95 @@ import loading from './loading.gif'
 const axios = require('axios');
 
 
+class Toast extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.fadeOut = this.fadeOut.bind(this);
+    this.state = {
+      hidden: false
+    }
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(this.fadeOut, 5000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+    this.interval = null;
+  }
+
+  fadeOut() {
+    this.setState({
+      hidden: true
+    })
+  }
+
+  render() {
+    return (
+      <div className={(this.state.hidden) ? "toast hidden" : "toast visible"} tabIndex="100">
+        {this.props.message}
+      </div >
+    )
+  }
+}
+
+
 class Loginform extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      message: ""
+      message: "",
+      passwordInput: "",
+      nameInput: "",
+      newAccount: false
     }
     this.handleProceedButton = this.handleProceedButton.bind(this)
+    this.handleNameChange = this.handleNameChange.bind(this)
+    this.handlePasswordChange = this.handlePasswordChange.bind(this)
+    this.handleNewAccountChange = this.handleNewAccountChange.bind(this)
+
+
+
   }
   handleProceedButton() {
-    this.props.handleProceedButton("username", "password");
+    this.props.handleProceedButton(this.state.nameInput, this.state.passwordInput, this.state.newAccount);
     // @todo fix above
   }
+
+  handleNameChange(e) {
+    this.setState({
+      nameInput: e.target.value
+    })
+
+  }
+  handlePasswordChange(e) {
+    this.setState({
+      passwordInput: e.target.value
+    })
+  }
+
+  handleNewAccountChange(e) {
+    this.setState({
+      newAccount: e.target.checked
+    })
+  }
+
   render() {
     return (
+
       <div className="chat-app container login">
+        <Toast message='This application is still in development' />
         <h1 className="login-text">Welcome to Chat</h1>
         <p className="login-subtext">Login or Create an Account to start chatting.<br /> Enter your username and password or create new ones, and an account will be created for you.</p>
-        <input className="login-field theme-input" placeholder="Full Name" maxLength="64" />
-        <input className="login-field theme-input" type="password" placeholder="Password" maxLength="128" />
+        <input className="login-field theme-input" placeholder="Full Name" maxLength="64" value={this.state.nameInput} onChange={this.handleNameChange} />
+        <input className="login-field theme-input" type="password" placeholder="Password" maxLength="128" value={this.state.passwordInput} onChange={this.handlePasswordChange} />
         <br />
         <button className="theme-btn login-field" onClick={this.handleProceedButton}>Proceed</button>
+        <br />
+        <input type="checkbox" class="checkbox" id="new-account" checked={this.state.newAccount} onChange={this.handleNewAccountChange} />
+        <label for="new-account">&nbsp;Create a new account</label>
         <br />
         <p className="message">{this.props.message}</p>
         <small>&copy; 2019 Diyanshu Agrawal. All rights reserved.</small>
@@ -226,6 +294,7 @@ class ChatApp extends React.Component {
     super(props);
     this.activate = this.activate.bind(this);
     this.state = {
+      toast: null,
       active: null,
       globalSettings: {
         name: "Divyanshu Agrawal",
@@ -248,7 +317,7 @@ class ChatApp extends React.Component {
 
   }
 
-  handleProceedButton(username, password) {
+  handleProceedButton(name, password, newAccount) {
     this.setState({
       loginFormMessage: (
         <span>
@@ -259,26 +328,44 @@ class ChatApp extends React.Component {
     })
     console.log(this.state.globalSettings.serverRoot)
     axios.post(this.state.globalSettings.serverRoot + "login", {
-      username: null,
-      password: null,
+      name: name,
+      password: password,
+      newAccount: newAccount,
       withCredentials: true,
     }, {
         withCredentials: true
       }).then(
         (response) => {
+          if (response.data.error) {
+            this.setState({
+              loginFormMessage: response.data.error
+            })
+            console.error("Login attempt response error : ", response.data);
+            return;
+          }
           const seti = this.state.globalSettings;
           seti.loggedIn = true;
           this.setState({
             globalSettings: seti,
+            newAccount: response.data.newAccount
           })
 
           axios.get(`${this.state.globalSettings.serverRoot}my-chats`, {}, {
             withCredentials: true
           }).then((response) => {
-            const newState = this.state;
-            newState.chats = response.data;
-            this.setState(newState);
-            console.log("Loaded Chats : ", response.data)
+            if (response.data.error) {
+              this.setState({
+                toast: "An error occurred while trying to load your chats\n, please try again."
+              })
+              console.error("Login", response.data.error)
+              return;
+            } else {
+              const newState = this.state;
+              newState.chats = response.data;
+              this.setState(newState);
+              console.log("Loaded Chats : ", response.data)
+            }
+
           }).catch(function (error) {
             alert("An error occured while fetching your chats, please reload the app");
             console.error("Fetching chats", error);
@@ -294,11 +381,9 @@ class ChatApp extends React.Component {
             withCredentials: true
           })
       }).then(function (response) {
-        console.log("CHECK", response)
       })
-
-
   }
+
 
 
   activate(name) {
@@ -308,6 +393,15 @@ class ChatApp extends React.Component {
   }
 
   render() {
+    var toast = null;
+    if (this.state.toast) {
+      toast = <Toast message={this.state.toast} />
+    }
+    var additionalRenders;
+    if (this.state.newAccount) {
+      additionalRenders = <Toast message="A new account was created for you. Welcome to Chat" />;
+      console.log("New Account")
+    }
     if (!this.state.globalSettings.loggedIn) {
       return (
         <Loginform globalSettings={this.state.globalSettings} handleProceedButton={this.handleProceedButton} message={this.state.loginFormMessage}></Loginform>
@@ -323,13 +417,13 @@ class ChatApp extends React.Component {
         chats.push(<p className="no-chats text-center padding-10 color-white">
           <img className="loading" src={loading} alt="..." />
           Downloading conversations...</p>)
-        console.error('OOF')
       } else {
         console.log(chats);
       }
 
       return (
         <div className="chat-app container">
+          {additionalRenders}
           <div className="row app-row">
             <div className="col-4 chats">
               <div className="chats-inner">
