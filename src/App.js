8fd183da359deab13286e7acf05a3f5eec29fae8
Divyanshu Agrawal <divyanshu123@hotmail.com>
2019-07-04 00:49:45 +0530

    Add chats from backed fetch feature

--- src/App.js
+++ src/App.js
@@ -92,8 +92,8 @@ class Loginform extends React.Component {
         <br />
         <button className="theme-btn login-field" onClick={this.handleProceedButton}>Proceed</button>
         <br />
-        <input type="checkbox" class="checkbox" id="new-account" checked={this.state.newAccount} onChange={this.handleNewAccountChange} />
-        <label for="new-account">&nbsp;Create a new account</label>
+        <input type="checkbox" className="checkbox" id="new-account" checked={this.state.newAccount} onChange={this.handleNewAccountChange} />
+        <label htmlFor="new-account">&nbsp;Create a new account</label>
         <br />
         <p className="message">{this.props.message}</p>
         <small>&copy; 2019 Diyanshu Agrawal. All rights reserved.</small>
@@ -103,137 +103,32 @@ class Loginform extends React.Component {
   }
 }
 
-const globalChats = [
-  {
-    name: "Akul Singhal",
-    messages: [
-      {
-        text: "Hello Divyanshu",
-        from: "Akul Singhal",
-        id: "csefvwvrvwe"
-      }, {
-        text: "Hi Akul",
-        from: "Divyanshu Agrawal",
-        id: "Dcswfcwefcwe"
-      },
-      {
-        text: "Hello Divyanshu",
-        from: "Akul Singhal",
-        id: "csefvlwvrvwe"
-      }, {
-        text: "Hi Akul",
-        from: "Divyanshu Agrawal",
-        id: "Dcswllfcwefcwe"
-      },
-      {
-        text: "Hello Divyanshu",
-        from: "Akul Singhal",
-        id: "csehfvwvrvwe"
-      }, {
-        text: "Hi Akul",
-        from: "Divyanshu Agrawal",
-        id: "Dcuhbswfcwefcwe"
-      },
-      {
-        text: "Hello Divyanshu",
-        from: "Akul Singhal",
-        id: "csefvuiygvwvrvwe"
-      }, {
-        text: "Hi Akul",
-        from: "Divyanshu Agrawal",
-        id: "Dcswbouyfcwefcwe"
-      },
-      {
-        text: "Hello Divyanshu",
-        from: "Akul Singhal",
-        id: "csefiupvwvrvwe"
-      }, {
-        text: "Hi Akul",
-        from: "Divyanshu Agrawal",
-        id: "Dcswp,ofcwefcwe"
-      },
-      {
-        text: "Hello Divyanshu",
-        from: "Akul Singhal",
-        id: "csefvp,owvrvwe"
-      }, {
-        text: "Hi Akul",
-        from: "Divyanshu Agrawal",
-        id: "Dcswvufcwefcwe"
-      },
-      {
-        text: "Hello Divyanshu",
-        from: "Akul Singhal",
-        id: "cseftucvwvrvwe"
-      }, {
-        text: "Hi Akul",
-        from: "Divyanshu Agrawal",
-        id: "Dczewswfcwefcwe"
-      },
-      {
-        text: "Hello Divyanshu",
-        from: "Akul Singhal",
-        id: "csefjghvwvrvwe"
-      }, {
-        text: "Hi Akul",
-        from: "Divyanshu Agrawal",
-        id: "Dcsioywfcwefcwe"
-      },
-    ]
-  }, {
-    name: "Ashutosh Agrawal",
-    messages: [
-      {
-        text: "Hello Divyanshu",
-        from: "Ashutosh Agrawal",
-        id: "aweqfcwefcw"
-      }, {
-        text: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
-        from: "Divyanshu Agrawal",
-        id: "asdcsdacwfea"
-      }, {
-        text: "But of course , that is correct. 🙂🙂 .Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc. ",
-        from: "Ashutosh Agrawal",
-        id: "aweqfcwefcwkk"
-      }, {
-        text: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum",
-        from: "Divyanshu Agrawal",
-        id: "aweqfcwefcw0"
-      }, {
-        text: "Bye",
-        from: "Ashutosh Agrawal",
-        id: "aweqfcwefcw,"
-      },
-    ]
-  }, {
-    name: "Aditi Pathak",
-    messages: []
-  }
-];
-
 class Chatbox extends React.Component {
 
   render() {
-    if (globalChats.find(obj => {
-      return obj.name === this.props.chat;
-    })) {
-      // Found stored chat in vars
-      const chatbubbles = [];
-      var chatIndex = null;
-      for (var index in globalChats) {
-        if (globalChats[index].name === this.props.chat) {
-          chatIndex = index;
-        }
-      }
-      for (const message of globalChats[chatIndex].messages) {
-        const bubbleClassName = "chat-bubble " + (message.from === this.props.globalSettings.name ? "sent" : "received");
+    const chatbubbles = [];
+    if (!this.props.chat) {
+      return (<div className="chatbox " >
+        <div className="bubble-container">
+          <p className  ="text-center white-texr">Choose a conversation to show messages.</p>
+
+        </div>
+        <div className="chat-text-box-container">
+          <input className="chat-text-box" placeholder="Enter your message here" />
+          <button className="option-button" title="Send smiling face">🙂</button>
+          <div className="clearfix"></div>
+        </div>
+      </div >)
+    } else {
+      for (const message of this.props.chat.messages) {
+        const bubbleClassName = "chat-bubble " + (message.from === this.props.name ? "received" : "sent");
         chatbubbles.push(
-          <div className={bubbleClassName} key={message.id}>
+          <div className={bubbleClassName} key={message._id}>
             <div className="chat-bubble-info">
               {message.from}
             </div>
             <div className="chat-bubble-message">
-              {message.text}
+              {message.message}
             </div>
 
 
@@ -254,19 +149,9 @@ class Chatbox extends React.Component {
         </div>
 
       )
-    } else if (!this.props.chat) {
-      // Chat is null
-      return (
-        <p className="no-chat-selected">Select a name on the side to begin chatting.</p>
-      )
-    } else {
-      //Chat exists but not downloaded.
-      return (
-        <p className="no-chat-selected">Downloading messages.</p>
-      )
-
-
     }
+
+
   }
 }
 
@@ -296,6 +181,7 @@ class ChatApp extends React.Component {
     this.state = {
       toast: null,
       active: null,
+      activeChatIndex: -1,
       globalSettings: {
         name: "Divyanshu Agrawal",
         id: null,
@@ -326,7 +212,6 @@ class ChatApp extends React.Component {
         </span>
       )
     })
-    console.log(this.state.globalSettings.serverRoot)
     axios.post(this.state.globalSettings.serverRoot + "login", {
       name: name,
       password: password,
@@ -350,37 +235,35 @@ class ChatApp extends React.Component {
             newAccount: response.data.newAccount
           })
 
-          axios.get(`${this.state.globalSettings.serverRoot}my-chats`, {}, {
+          axios.get(`${this.state.globalSettings.serverRoot}my-chats`, {
             withCredentials: true
-          }).then((response) => {
-            if (response.data.error) {
-              this.setState({
-                toast: "An error occurred while trying to load your chats\n, please try again."
-              })
-              console.error("Login", response.data.error)
-              return;
-            } else {
-              const newState = this.state;
-              newState.chats = response.data;
-              this.setState(newState);
-              console.log("Loaded Chats : ", response.data)
-            }
+          }, {
+              withCredentials: true
+            }).then((response) => {
+              if (response.data.error) {
+                this.setState({
+                  toast: "An error occurred while trying to load your chats\n, please try again."
+                })
+                console.error("Login", response.data.error)
+                return;
+              } else {
+                const newState = this.state;
+                newState.chats = response.data;
+                this.setState(newState);
+              }
 
-          }).catch(function (error) {
-            alert("An error occured while fetching your chats, please reload the app");
-            console.error("Fetching chats", error);
-          })
+            }).catch(function (error) {
+              alert("An error occured while fetching your chats, please reload the app");
+              console.error("Fetching chats", error);
+            })
 
         }
       ).catch(function (error) {
         console.error("OOPS", error)
-      }).finally(() => {
-        return axios.get(this.state.globalSettings.serverRoot, {
-          withCredentials: true
-        }, {
-            withCredentials: true
-          })
-      }).then(function (response) {
+        this.setState({
+          toast: error
+        })
+
       })
   }
 
@@ -390,13 +273,21 @@ class ChatApp extends React.Component {
     this.setState({
       active: name
     })
+    var index = -1;
+    this.state.chats.find(function (item, i) {
+      if (item.name === name) {
+        index = i;
+        return i;
+      } else {
+        return -1;
+      }
+    })
+    this.setState({
+      activeChatIndex: index
+    })
   }
 
   render() {
-    var toast = null;
-    if (this.state.toast) {
-      toast = <Toast message={this.state.toast} />
-    }
     var additionalRenders;
     if (this.state.newAccount) {
       additionalRenders = <Toast message="A new account was created for you. Welcome to Chat" />;
@@ -407,18 +298,18 @@ class ChatApp extends React.Component {
         <Loginform globalSettings={this.state.globalSettings} handleProceedButton={this.handleProceedButton} message={this.state.loginFormMessage}></Loginform>
       )
     } else {
-      const chats = [];
+      var chats = [];
       for (var element of this.state.chats) {
         chats.push(
           <Chat name={element.name} onActivate={this.activate} globalSettings={this.state.globalSettings} active={(element.name === this.state.active)} key={element.name} />
         )
       }
       if (chats.length === 0) {
-        chats.push(<p className="no-chats text-center padding-10 color-white">
+        chats = <p className="no-chats text-center padding-10 color-white">
           <img className="loading" src={loading} alt="..." />
-          Downloading conversations...</p>)
+          Downloading conversations...</p>
       } else {
-        console.log(chats);
+
       }
 
       return (
@@ -436,7 +327,7 @@ class ChatApp extends React.Component {
 
             </div>
             <div className="col-8">
-              <Chatbox chat={this.state.active} globalSettings={this.state.globalSettings}></Chatbox>
+              <Chatbox name={this.state.active} globalSettings={this.state.globalSettings} chat={this.state.chats[this.state.activeChatIndex]}></Chatbox>
             </div>
           </div>
 
@@ -445,10 +336,6 @@ class ChatApp extends React.Component {
     }
   }
 }
-
-var friends = [
-  "Akul", "Harshit", "Aditi"
-]
 
 function App() {
   return (
