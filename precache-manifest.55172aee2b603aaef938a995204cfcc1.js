f769ed0801a29aaa99fad50c8a1fb277599eb382
Divyanshu Agrawal <divyanshu123@hotmail.com>
2019-07-05 01:16:10 +0530

    Updates

---
+++ precache-manifest.55172aee2b603aaef938a995204cfcc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bee6c6550b4e841b9c46938b064e1ed5",
+    "url": "/chatapp/index.html"
+  },
+  {
+    "revision": "66718ca174afbbe849be",
+    "url": "/chatapp/static/css/main.c195ca23.chunk.css"
+  },
+  {
+    "revision": "6460adc8f08a9f619d46",
+    "url": "/chatapp/static/js/2.4b42bcf5.chunk.js"
+  },
+  {
+    "revision": "66718ca174afbbe849be",
+    "url": "/chatapp/static/js/main.ac1fbd8b.chunk.js"
+  },
+  {
+    "revision": "a9ac13511f8ef41607bf",
+    "url": "/chatapp/static/js/runtime~main.e147c499.js"
+  },
+  {
+    "revision": "e6ebdebf12eac3d3f79c37b6893b99be",
+    "url": "/chatapp/static/media/loading.e6ebdebf.gif"
+  }
+]);
